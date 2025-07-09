27e89006d02f56fae1d1be4b1102583994ac2e06
etcq <resonans93@gmail.com>
2025-07-09 23:49:31 +0500

    feat: trying add marvel api

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import style from './card-list.module.scss';
+import md5 from 'md5';
 
 class CardList extends React.Component {
+  publicKey: string = String(import.meta.env.VITE_PUBLIC_KEY);
+  privateKey: string = String(import.meta.env.VITE_PRIVATE_KEY);
+  apiBase: string = 'https://gateway.marvel.com:443/v1/public/';
+
+  getCharacters = (): void => {
+    const time = new Date().getTime();
+    const hash = this.generateHash(time);
+    const url = `${this.apiBase}/characters?apikey=${this.publicKey}&hash=${hash}&ts=${time}&limit=20&offset=0`;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  generateHash = (time: number): string => {
+    return md5(time + this.privateKey + this.publicKey);
+  };
   render(): React.ReactNode {
-    return <div className={style['card-list']}></div>;
+    return (
+      <div className={style['card-list']}>
+        <h1>Marvel Characters</h1>
+        <button onClick={this.getCharacters}>Fetch Characters</button>
+        <p>Check the console for the fetched data.</p>
+      </div>
+    );
   }
 }
 
---
+++ src/components/card-list/card/card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <img src="#" alt="Card image" />
+        <h2>Spider-Man</h2>
+        <p>This is a simple card component.</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
