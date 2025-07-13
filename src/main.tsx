1983099a12ff5939aacb921853de4519c05b460d
etcq <resonans93@gmail.com>
2025-07-13 10:22:25 +0500

    refactor: change eslint config

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './assets/style/global.scss';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
