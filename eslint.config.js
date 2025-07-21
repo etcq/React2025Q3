d23e75642d7018e273e78c0f22999dd4191dfda3
etcq <resonans93@gmail.com>
2025-07-21 12:15:52 +0500

    refactor: add coverage folder to eslint ignore

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ['dist', 'node_modules', 'coverage'] },
   {
     extends: [
       js.configs.recommended,
