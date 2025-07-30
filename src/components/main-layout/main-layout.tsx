520c6da09780e609f99515bcec1f8645092182a7
etcq <resonans93@gmail.com>
2025-07-30 16:46:03 +0500

    feat: add context

--- src/components/main-layout/main-layout.tsx
+++ src/components/main-layout/main-layout.tsx
@@ -1,22 +1,26 @@
-import { type FC, Suspense } from 'react';
+import { type FC, Suspense, use } from 'react';
 import style from './main-layout.module.scss';
 import { Outlet } from 'react-router';
 import Loading from '../loading/loading';
 import bgPath from '../../assets/image/rick-and-morty-bg.jpg';
 import { Header } from '../header/header';
+import ThemeContext from '../../core/contexts/contexts';
 
 export const MainLayout: FC = () => {
+  const theme = use(ThemeContext);
   return (
-    <div
-      className={style.layout}
-      style={{ background: `url(${bgPath}) center center/cover no-repeat` }}
-    >
-      <Header />
-      <main className={style['layout-content']}>
-        <Suspense fallback={<Loading />}>
-          <Outlet />
-        </Suspense>
-      </main>
-    </div>
+    <ThemeContext value={theme}>
+      <div
+        className={style.layout}
+        style={{ background: `url(${bgPath}) center center/cover no-repeat` }}
+      >
+        <Header />
+        <main className={style['layout-content']}>
+          <Suspense fallback={<Loading />}>
+            <Outlet />
+          </Suspense>
+        </main>
+      </div>
+    </ThemeContext>
   );
 };
